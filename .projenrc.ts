aae61ca4bbe10bab1b770f956d7e4463d2783464
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2026-08-03 23:50:29 +0200

    fix

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,10 +7,6 @@ const project = new TypescriptApplicationProject({
   name: 'auto-approve-empty-changeset-pipeline',
   devDeps: ['aws-cdk', 'aws-cdk-lib', 'constructs', 'esbuild', '@types/aws-lambda'],
   deps: ['@aws-sdk/client-codepipeline', '@aws-sdk/client-cloudformation', '@aws-lambda-powertools/logger'],
-  scripts: {
-    'cdk:pipeline': 'pnpm cdk --app \'ts-node src/bin/pipeline.ts\'',
-    'cdk:app': 'pnpm cdk --app \'ts-node src/bin/app.ts\'',
-  },
   gitignore: ['cdk.out'],
   eslintOptions: {
     dirs: ['.'],
@@ -21,6 +17,12 @@ const project = new TypescriptApplicationProject({
       skipLibCheck: true,
     },
   },
+});
+project.addTask('cdk:pipeline', {
+  exec: 'pnpm cdk --app \'ts-node src/bin/pipeline.ts\'',
+});
+project.addTask('cdk:app', {
+  exec: 'pnpm cdk --app \'ts-node src/bin/app.ts\'',
 });
 
 new TextFile(project, '.pnpmfile.mjs', {
